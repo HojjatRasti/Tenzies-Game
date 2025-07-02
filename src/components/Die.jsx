6579884e494fb1f356ad31caca26d405cb7e916e
unknown <hojjatrasti1380@gmail.com>
2025-07-02 19:33:38 +0330

    A realy simplt Tenzes game :)

---
+++ src/components/Die.jsx
@@ -0,0 +1,14 @@
+export default function Die (props) {
+   const styles ={
+      backgroundColor: props.isHeld ? '#59e391' : ''
+   } 
+   return(
+    <button 
+      style={ styles }
+      onClick={props.hold} 
+       aria-pressed={props.isHeld}
+       aria-label={`Die with value ${props.value}, 
+         ${props.isHeld ? "held" : "not held"}`}
+    >{props.value}</button>
+   )
+}
